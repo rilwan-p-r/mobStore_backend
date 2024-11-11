e34637df7be67380451ee47079e8b850c9404372
rilwan-p-r <rilwanpr@gmail.com>
2024-11-11 05:59:00 +0530

    feat: Set cookie and CORS settings based on environment (prod/dev)

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken';
-export const generateToken =(res,userId,tokenName)=>{
-    const token = jwt.sign({userId},process.env.SECRET_KEY,{
-        expiresIn:'1d'
+
+export const generateToken = (res, userId, tokenName) => {
+    const token = jwt.sign({ userId }, process.env.SECRET_KEY, {
+        expiresIn: '1d'
     });
-    res.cookie(tokenName,token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
-        sameSite:'Strict',
-        maxAge: 24 * 60 * 60 * 1000
-    })
-}
+
+    res.cookie(tokenName, token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
+        maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
+};
